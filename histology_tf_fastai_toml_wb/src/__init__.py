7d9e58ed2f4ae54ef1ae0c9392ea559673732f40
Diogen Babuc <54851778+fortunab@users.noreply.github.com>
2026-09-08 12:22:17 +0300

    Add files via upload

---
+++ histology_tf_fastai_toml_wb/src/__init__.py
@@ -0,0 +1,28 @@
+"""
+Colorectal Histology fastai + TOML + Weights & Biases (W&B) Suite.
+"""
+
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+# Compatibility patch for fastcore & PyTorch / Python 3.12+ (read-only __doc__ on C-extension functions)
+try:
+    import fastcore.foundation
+
+    def _safe_add_docs(cls, cls_doc=None, **docs):
+        if cls_doc is not None:
+            try:
+                cls.__doc__ = cls_doc
+            except (AttributeError, TypeError):
+                pass
+        for k, v in docs.items():
+            try:
+                f = getattr(cls, k)
+                f.__doc__ = v
+            except (AttributeError, TypeError):
+                pass
+
+    fastcore.foundation.add_docs = _safe_add_docs
+except Exception:
+    pass
