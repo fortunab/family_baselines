86883677a67f6d43bb65906982d635b450c6f745
Diogen Babuc <54851778+fortunab@users.noreply.github.com>
2026-09-09 10:38:51 +0200

    Update run_all.sh

--- histology_tf_skorch_wb/run_all.sh
+++ histology_tf_skorch_wb/run_all.sh
@@ -1,5 +1,3 @@
-cd /mnt/c/Users/Lenovo/.gemini/antigravity/scratch/colorectal_histology_skorch_toml_wandb
-
 # 1. Virtual Environment Setup
 python3 -m venv venv_skorch_toml
 source venv_skorch_toml/bin/activate
@@ -7,7 +5,6 @@ source venv_skorch_toml/bin/activate
 # 2. Install Dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
 
 # 3. Run Training
 python3 main_skorch_wandb.py --config configs/convnext_base.toml
